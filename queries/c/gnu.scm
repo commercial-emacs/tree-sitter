8506b87cf453b11d8a4a80824a39f7f126318a0e
dickmao <none>
2022-11-30 17:41:43 -0500

    indent inheritance

--- queries/c/gnu.scm
+++ queries/c/gnu.scm
@@ -1,21 +1,6 @@
-[
-  (compound_statement)
-  (preproc_arg)
-  (field_declaration_list)
-  (case_statement)
-  (enumerator_list)
-  (struct_specifier)
-  (compound_literal_expression)
-  (initializer_list)
-  (while_statement)
-  (for_statement)
-  (switch_statement)
-  (expression_statement)
-] @indent
-(if_statement condition: (_) @indent)
+; inherits: c
+
 ((if_statement consequence: (_)) @indent)
-(init_declarator) @indent
-(compound_statement "}" @indent_end)
 
 "else" @dedent
 
@@ -25,29 +10,6 @@
   (statement_identifier)
 ] @branch
 
-[
-  "#define"
-  "#ifdef"
-  "#if"
-  "#else"
-  "#endif"
-] @zero_indent
-
-[
-  (preproc_arg)
-  (string_literal)
-] @ignore
-
-((ERROR (parameter_declaration)) @aligned_indent
- (#set! "delimiter" "()"))
-([(argument_list) (parameter_list)] @aligned_indent
- (#set! "delimiter" "()"))
 ((parenthesized_expression) @aligned_indent
  (#set! "delimiter" "()"))
-((for_statement initializer: (_)) @aligned_indent
- (#set! "delimiter" "()"))
-((for_statement condition: (_)) @aligned_indent
- (#set! "delimiter" "()"))
-((for_statement update: (_)) @aligned_indent
- (#set! "delimiter" "()"))
-(comment) @auto
+((for_statement) @aligned_indent (#set! "delimiter" "()"))
