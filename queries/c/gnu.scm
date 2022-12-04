91c2c90e28434ead77cde064ebf54a1937b2d06f
dickmao <none>
2022-12-04 12:36:39 -0500

    initializer_list indent

--- queries/c/gnu.scm
+++ queries/c/gnu.scm
@@ -4,16 +4,13 @@
  (conditional_expression)
  (return_statement)
 ] @indent
+
 ((if_statement consequence: (_)) @indent)
+((initializer_list members: (_)) @indent)
 
 "else" @dedent
 
-[
-  ")"
-  "}"
-  (statement_identifier)
-] @branch
-
 ((parenthesized_expression) @aligned_indent
  (#set! "delimiter" "()"))
-((for_statement) @aligned_indent (#set! "delimiter" "()"))
+((for_statement) @aligned_indent
+ (#set! "delimiter" "()"))
